c3f37b1c279ad8cf235daa04f275ea79613fdac4
zanozano <zanozanozano@gmail.com>
2023-05-09 19:52:41 +0700

    adding new files

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,7 @@
+class NewsController < ApplicationController
+  def index
+  end
+
+  def blog
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/about'
+
+get '/technology', to: 'news#index'
+
+get '/blog', to: 'news#blog'
+
+end
+ 
